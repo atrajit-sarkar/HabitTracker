618afd0eb62774badf50f0866b6a12f7a927d0f2
Atrajit Sarkar <atrajit.sarkar@alumni.iitd.ac.in>
2025-10-02 19:47:38 +0530

    feat: Update version to 3.0.4 with incremented version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.habittracker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "3.0.3"
+        versionCode = 5
+        versionName = "3.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
