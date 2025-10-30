ca3d8f1fca3881c74ccd8608a1d9af4c43734832
Atrajit Sarkar <atrajit.sarkar@alumni.iitd.ac.in>
2025-10-30 13:18:30 +0530

    Update version code to 33 and version name to 7.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "it.atraj.habittracker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 32
-        versionName = "7.1.3"
+        versionCode = 33
+        versionName = "7.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
